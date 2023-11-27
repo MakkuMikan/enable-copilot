8f73f71c1783cf4b8b43b662eeadc3bf3a4719ea
Jack McGinnis <42243844+MakkuMikan@users.noreply.github.com>
2023-11-27 09:40:08 +0000

    Create enable-copilot.ps1

---
+++ enable-copilot.ps1
@@ -0,0 +1,28 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+
+Add-Type -AssemblyName System.Windows.Forms
+
+Push-Location $env:TEMP
+Invoke-WebRequest "https://github.com/thebookisclosed/ViVe/releases/download/v0.3.3/ViVeTool-v0.3.3.zip" -OutFile ".\vivetool.zip"
+Expand-Archive ".\vivetool.zip" -DestinationPath ".\vivetool"
+
+Push-Location ".\vivetool"
+.\ViVeTool.exe /enable /id:44774629,44776738,44850061,42105254,41655236
+Pop-Location
+
+Remove-Item -Recurse -Force ".\vivetool"
+Remove-Item ".\vivetool.zip"
+Pop-Location
+
+$ConfirmBox = [System.Windows.MessageBox]::Show("Would you like to reboot your computer to enable Windows Copilot?", "Shutdown Confirmation",  [System.Windows.MessageBoxButton]::YesNo)
+
+If ($ConfirmBox -eq "Yes") {
+  Restart-Computer
+}
